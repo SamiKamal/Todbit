c396eae62af7f6ab326096d004d3ef5031072cf2
Sami <samixgames@hotmail.com>
2019-06-21 10:28:44 +0300

    Set-up env

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill','./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
     },
     devServer: {
         contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                // every file eneds with .js
+                test:/\.js$/,
+                // exclude the files in node_modules dir
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     }
 }
