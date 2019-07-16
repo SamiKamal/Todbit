d5ec41efd4b3475562397e08d652087e255bfda9
Sami <samixgames@hotmail.com>
2019-07-16 15:43:45 +0300

    responsive nav

--- src/js/bog.js
+++ src/js/bog.js
@@ -1,1 +1,20 @@
-export var brd = 'boga'
+document.getElementById('nav-toggle').addEventListener('click', (e) => {
+    document.getElementById('nav-toggle').classList.toggle('active');
+
+    toggleNav();
+})
+
+function toggleNav() {
+    if (document.querySelector('body').classList.contains('show-nav')){
+        document.querySelector('body').classList.remove('show-nav')
+        document.querySelector('nav').classList.remove('pull-nav')
+        // document.querySelector('nav').style.display = 'none'
+        
+    
+} else {
+    document.querySelector('body').classList.add('show-nav')
+    // document.querySelector('nav').classList.add('pull-nav')
+    // document.querySelector('nav').style.display = 'block'
+
+}
+}
